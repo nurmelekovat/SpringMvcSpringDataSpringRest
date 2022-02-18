ea6d20cea363bd352d4f7442106fbecd017abc56
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-18 16:56:43 +0300

    form validation full

--- src/main/java/com/innova/controller/FormValidationController.java
+++ src/main/java/com/innova/controller/FormValidationController.java
@@ -1,16 +1,18 @@
 package com.innova.controller;
-
+//import com.innova.dto.InnovaValidationDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
 @Log4j2
 public class FormValidationController {
-
     // http://localhost:8080/formurl
     @GetMapping("/formurl")
     public String getForm(Model model) {
@@ -20,9 +22,16 @@ public class FormValidationController {
 
     // http://localhost:8080/formurl
     @PostMapping("/formurl")
-    public String postForm( @ModelAttribute("key_form") InnovaValidationDto dto ) {
-        log.info(dto);
-        return "formvalidation";
-    }
+        public String postForm(@Valid @ModelAttribute("key_form") InnovaValidationDto dto, BindingResult bindingResult) {
 
-}
+            if(bindingResult.hasErrors()){
+                log.error("Hata var");
+                log.info(dto);
+                return "formvalidation";
+            }
+            //database yazılacak alan
+            log.info(dto);
+            return "success";
+        }
+
+    }
