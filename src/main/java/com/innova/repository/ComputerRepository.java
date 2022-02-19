d45376f8d9b34efefd187cdc31f013439df05c33
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-19 16:48:20 +0300

    select

--- src/main/java/com/innova/repository/ComputerRepository.java
+++ src/main/java/com/innova/repository/ComputerRepository.java
@@ -3,7 +3,16 @@ package com.innova.repository;
 import com.innova.entity.ComputerEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ComputerRepository extends CrudRepository<ComputerEntity,Long> {
 
+    //1.YOL
     //Register MVC thymeleaf, Entity register, Database ekleyeğiz
+    List<ComputerEntity>  findComputerEntitiesByComputerName(String computerName);
+
+    //2.YOL
+    //@Query("select c from ComputerEntity c where c.computerName=:computerName")
+    //List<ComputerEntity> findComputerEntitiesByComputerName(@Param("computerName") String computerName);
+
 }
