d8529a280afb8f494aac41f8591118eda4286708
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-19 17:04:22 +0300

    DeliveredQuery

--- src/main/java/com/innova/controller/ComputerController.java
+++ src/main/java/com/innova/controller/ComputerController.java
@@ -143,15 +143,15 @@ public class ComputerController {
         return iterableList+"\n";
     }
     ////KEndi repository kullanacağım
-    //@Autowired
-    //MyRepositoryImp myRepositoryImp;
+    @Autowired
+    MyRepositoryImp myRepositoryImp;
 
     //SELECT
     // http://localhost:8080/computer/select/minprice/66
     @GetMapping("/computer/select/minprice/{price}")
     @ResponseBody
     public String getComputerSelectPriceMin( @PathVariable(name="price") double priceTotal ){
-        Iterable<ComputerEntity> iterableList= computerRepository.findComputerPriceMin(priceTotal);
+        Iterable<ComputerEntity> iterableList= myRepositoryImp.findComputerPriceMin(priceTotal);
         for(ComputerEntity temp :iterableList){
             log.info(temp);
         }
