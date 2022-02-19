1705776bd80aba8ace55dcee23e001980e99f6c5
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-19 17:06:49 +0300

    sorting , paging ,sortingandpaging

---
+++ src/main/java/com/innova/repository/IPersonRepository.java
@@ -0,0 +1,7 @@
+package com.innova.repository;
+
+import com.innova.entity.PersonEntity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface IPersonRepository extends PagingAndSortingRepository<PersonEntity,Long> {
+}
