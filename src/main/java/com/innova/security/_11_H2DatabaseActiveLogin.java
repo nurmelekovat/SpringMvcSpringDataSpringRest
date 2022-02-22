e95e5c42cd336c39525a8488de666401b9886e03
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-22 23:06:03 +0300

    H2-dB aktif etmek için kulanıyoruz.

--- src/main/java/com/innova/security/_11_H2DatabaseActiveLogin.java
+++ src/main/java/com/innova/security/_11_H2DatabaseActiveLogin.java
@@ -3,19 +3,23 @@ package com.innova.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@EnableWebSecurity
-public class _10_Login extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class _11_H2DatabaseActiveLogin extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/security/public","/security/success") //bu @GetMapping izin verdiğim url
+                .antMatchers("/security/public", "/security/success") //bu @GetMapping izin verdiğim url
                 .permitAll()//yukarıdaki asyfalara login olmadan izin ver
                 .antMatchers("/login").permitAll()
                 .antMatchers("/logout").permitAll()
+
+                .antMatchers("/h2-console/**").permitAll() //H2 active
+
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -27,6 +31,10 @@ public class _10_Login extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/logout")
                 .invalidateHttpSession(true)
                 .permitAll();
+
+        http.csrf().disable();                   //H2 active
+        http.headers().frameOptions().disable();  //H2 active
+//         http://localhost:8080/h2-console
     }
 
 
