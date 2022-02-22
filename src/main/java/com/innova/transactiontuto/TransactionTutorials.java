425c16edad9db78e0853a69a737149deedb97ef5
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-22 23:04:38 +0300

    application.properties revize

--- src/main/java/com/innova/transactiontuto/TransactionTutorials.java
+++ src/main/java/com/innova/transactiontuto/TransactionTutorials.java
@@ -1,9 +1,6 @@
 package com.innova.transactiontuto;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RestController;
 
 
 
