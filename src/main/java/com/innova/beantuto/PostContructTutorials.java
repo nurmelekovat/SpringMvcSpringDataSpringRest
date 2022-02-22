8ff881ab4f491a1568e5568247af7126fa7661b9
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-22 22:29:56 +0300

    404 500 sayfası

--- src/main/java/com/innova/beantuto/PostContructTutorials.java
+++ src/main/java/com/innova/beantuto/PostContructTutorials.java
@@ -2,7 +2,6 @@ package com.innova.beantuto;
 
 import lombok.Data;
 import lombok.ToString;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
